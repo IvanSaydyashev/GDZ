dee14070b24db1c75db73d5aa57fc57b31db2574
IvanSaydyashev <isarus2007@mail.ru>
2022-04-07 23:19:23 +0300

    q

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telebot import types
 from bs4 import BeautifulSoup
 import requests
 
-
+msg = ''
 token = '5008620120:AAG18aU566Sls5K41SdvZ0Jb0z9fRyo62dA'
 bot = telebot.TeleBot(token)
 
@@ -13,22 +13,14 @@ urlsrus = ['https://gdz.ru/class-8/russkii_yazik/trostencova-8/', 'https://gdz.r
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    keyboard = types.InlineKeyboardMarkup()
-    btnrus1 = types.InlineKeyboardButton(text='Тростнецова, Ладыженская', callback_data='rus1')
-    btnrus2 = types.InlineKeyboardButton('Бархударов, Крючков', callback_data='rus2')
-    btnrus3 = types.InlineKeyboardButton('Разумовская, Львова, Капинос', callback_data='rus3')
-    btnrus4 = types.InlineKeyboardButton('Рыбченкова', callback_data='rus4')
-    keyboard.add(btnrus1, btnrus2, btnrus3, btnrus4)
-    bot.send_message(message.chat.id, 'Выберите учебник', reply_markup=keyboard)
+    bot.send_message(message.chat.id, 'Введите номер упражнения')
 
-
-@bot.message_handler(content_types=['text'])
-def main(message):
-    global url, page
-    if message.text == 'Тростнецова, Ладыженская':
-        bot.send_message(message.chat.id, 'Введите номер упражнения')
-        url = f'https://gdz.ru/class-8/russkii_yazik/trostencova-8/{message.text}-nom/'
-    try:
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    global msg
+    if call.data == 'rus1':
+        print(msg)
+        url = f'https://gdz.ru/class-8/russkii_yazik/trostencova-8/{msg}-nom/'
         page = requests.get(url)
         soup = BeautifulSoup(page.text, "html.parser")
         imgs = soup.findAll('img')
@@ -36,10 +28,19 @@ def main(message):
             if img.has_attr('src'):
                 if 'tasks' in img['src']:
                     url_image = f'https:{img["src"]}'
-                    bot.send_photo(message.chat.id, photo=url_image)
-        bot.send_message(message.chat.id, 'Введите номер упражнения')
-    except:
-        pass
+                    bot.send_photo(call.message.chat.id, photo=url_image)
 
+
+@bot.message_handler(content_types=['text'])
+def main(message):
+    global msg
+    msg = message.text
+    keyboard = types.InlineKeyboardMarkup()
+    btnrus1 = types.InlineKeyboardButton(text='Тростнецова, Ладыженская', callback_data='rus1')
+    btnrus2 = types.InlineKeyboardButton(text='Бархударов, Крючков', callback_data='rus2')
+    btnrus3 = types.InlineKeyboardButton(text='Разумовская, Львова, Капинос', callback_data='rus3')
+    btnrus4 = types.InlineKeyboardButton(text='Рыбченкова', callback_data='rus4')
+    keyboard.add(btnrus1, btnrus2, btnrus3, btnrus4)
+    bot.send_message(message.chat.id, 'Выберите учебник', reply_markup=keyboard)
 
 bot.infinity_polling()
